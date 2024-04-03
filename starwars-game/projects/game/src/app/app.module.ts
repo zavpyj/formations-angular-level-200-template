3a4d5b85df2c7ae6fbbd71e09064af8251b951d2
Xavier Guille <xavier.guille@sib.fr>
2024-04-03 19:10:30 +0200

    HeaderComponent & HeaderModule (avec imports: [SearchComponent]) : SCAM

--- starwars-game/projects/game/src/app/app.module.ts
+++ starwars-game/projects/game/src/app/app.module.ts
@@ -5,15 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameModule } from './features/game/game.module';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { SearchComponent, SearchModule } from './shared/components/search/search.component';
+import { HeaderComponent, HeaderModule } from './shared/components/header/header.component';
 import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    // HeaderComponent,
     SideBarComponent
   ],
   imports: [
@@ -21,8 +20,9 @@ import { SideBarComponent } from './shared/components/side-bar/side-bar.componen
     AppRoutingModule,
     HttpClientModule,
     GameModule,
-    SearchModule,
-    SearchComponent
+    HeaderModule
+    // SearchModule, : jusqu'à angular 14
+    // SearchComponent : on va utiliser celui de librairie
   ],
   providers: [],
   bootstrap: [AppComponent, HeaderComponent, SideBarComponent] // disparait après angular 14 pour de nouveaux projets
