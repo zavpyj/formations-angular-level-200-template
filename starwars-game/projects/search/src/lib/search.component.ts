d7cef2eee46da96472ae31f1b3c30d001471605b
Xavier Guille <xavier.guille@sib.fr>
2024-04-03 14:09:54 +0200

    cd starwars-game; ng g lib search
    puis :
    $ ng build search
    Building Angular Package
    
    ------------------------------------------------------------------------------
    Building entry point 'search'
    ------------------------------------------------------------------------------
    ✔ Compiling with Angular sources in Ivy partial compilation mode.
    ✔ Generating FESM bundles
    ✔ Copying assets
    ✔ Writing package manifest
    ✔ Built search
    
    ------------------------------------------------------------------------------
    Built Angular Package
     - from: C:\Dev\projets\formationangular\formations-angular-level-200-template-main\starwars-game\projects\search
     - to:   C:\Dev\projets\formationangular\formations-angular-level-200-template-main\starwars-game\dist\search
    ------------------------------------------------------------------------------
    
    Build at: 2024-04-03T12:13:52.496Z - Time: 4307ms

---
+++ starwars-game/projects/search/src/lib/search.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-search',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      search works!
+    </p>
+  `,
+  styles: ``
+})
+export class SearchComponent {
+
+}
