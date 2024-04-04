4b4711fb6eb92e4430f72f9addc35c47e3ea8ab2
Xavier Guille <xavier.guille@sib.fr>
2024-04-04 15:17:31 +0200

    ng g c features/game/Stats --project=game

---
+++ starwars-game/projects/game/src/app/features/game/stats/stats.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'game-stats',
+  standalone: true,
+  imports: [],
+  templateUrl: './stats.component.html',
+  styleUrl: './stats.component.css'
+})
+export class StatsComponent {
+
+}
